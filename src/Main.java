ddc17120b447ecabded099b74e3e33842520650b
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-22 16:09:28 +0530

    number day

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int day=7;
+        if(day>=1 && day<=7)
+            if(day==1)
+                System.out.println("Sunday");
+            if(day==2)
+                System.out.println("Monday");
+            if(day==3)
+                System.out.println("Tuesday");
+            if(day==4)
+                System.out.println("Wednesday");
+            if(day==5)
+                System.out.println("Thursday");
+            if(day==6)
+                System.out.println("Friday");
+            if(day==7)
+                System.out.println("Saturday");
+        else System.out.println("invalid input");
+    }
+}
